a19cc46215d311763b1eda6c680e6eb3f513b50f
Jaime <jaime.valdez@potros.itson.edu.mx>
2023-02-17 20:21:39 -0700

    primer commit get started y login

---
+++ app/src/main/java/jaime/valdez/thecheezery/MenuActivity.kt
@@ -0,0 +1,11 @@
+package jaime.valdez.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
